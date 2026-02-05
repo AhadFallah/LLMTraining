2c60a2edd6e40f56edc43bfd31d457eb878c4d8e
mahan <ahad.fallah.k@gmail.com>
2026-02-05 23:15:57 +0330

    page_36

--- tokenizing/tokenizing.py
+++ tokenizing/tokenizing.py
@@ -13,3 +13,23 @@ preproccessed=re.split(r'([,.:;?_!"()\']|--|\s)',text)
 result = [item for item in preproccessed if item.strip()]
 # make the tokens unique and sort them
 all_words=sorted(set(result))
+#give id
+vocap={token:integer for integer,token in enumerate(all_words)}
+
+class SimpleTokenizerV1:
+    def __init__(self,vocap):
+        #same in put make the string to token ids
+        self.str_to_int=vocap
+        #this will make int first and string as value
+        self.int_to_str={i:s for s,i in vocap.items()}
+    def encode(self,text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\\s)', text)
+        result=[item for item in preproccessed if item.strip()]
+        ids=[self.str_to_int for s in result]
+        return ids
+    def decode(self,ids):
+        text=" ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\\s+([,.?!"()\\])', r'\\1', text) 
+        return text
+tokenrizer=SimpleTokenizerV1(vocap)
+print(tokenrizer.decode([14]))
